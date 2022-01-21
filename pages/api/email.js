e4ace82ef84bf311862df2dd18b8cfc549f88ce1
Emre <pi.emree@gmail.com>
2022-01-21 16:34:00 +0300

    Remove token.json

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,16 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from "axios";
-import fs from "fs";
-import path from "path";
 
-const tk = fs.readFileSync(path.join(__dirname, "../../../../", "token.json"));
-const t = JSON.parse(tk);
+let t = {};
 
 export default async function handler(req, res) {
   if (req.method == "POST") {
-    if (t.access_token == null || t.refresh_token == null) {
+    if (t.access_token) {
       const result = await login();
-      saveTokens(result.data.resultObject);
+      t = Object.assign(t, result.data.resultObject);
     }
 
     try {
@@ -18,22 +15,15 @@ export default async function handler(req, res) {
 
       return res.status(200).send(result1.data);
     } catch (error) {
-      if (error.data?.resultCode == 401 || error.response?.status == 401) {
-        try {
-          const result2 = await getRefreshToken();
-
-          saveTokens(result2.data.resultObject);
-
-          const result3 = await sendMail(req.body);
-
-          return res.status(200).send(result3.data);
-        } catch (error) {
-          console.log(error);
-          return res.status(500).send(error);
-        }
+      try {
+        const result = await login();
+        t = Object.assign(t, result.data.resultObject);
+        const result3 = await sendMail(req.body);
+        console.log(result3.data);
+        return res.status(200).send(result3.data);
+      } catch (error) {
+        return res.status(500).send(error);
       }
-      console.log(error);
-      return res.status(500).send(error);
     }
   }
 }
@@ -50,18 +40,5 @@ function login() {
   return axios.post("https://useapi.useinbox.com/token", {
     EmailAddress: "emre.demir@tazebt.com",
     Password: "Lftixyz1.",
-  });
-}
-
-function saveTokens(data) {
-  fs.writeFileSync(
-    path.join(__dirname, "../../../../", "token.json"),
-    JSON.stringify(data)
-  );
-}
-
-function getRefreshToken() {
-  return axios.post("https://useapi.useinbox.com/token/refresh", {
-    RefreshToken: t.refresh_token,
   });
 }
